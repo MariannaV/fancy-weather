5791a87a50805401e85d5263eee6947d51225480
marianna <vergunmari@gmail.com>
2020-05-16 23:57:14 +0300

    feat: add starter

---
+++ webpack.config.js
@@ -0,0 +1,79 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.js'
+  },
+  devtool: 'source-map', // Generate separate source map files
+  devServer: {
+    contentBase: './dist',
+    overlay: true // Show errors in overlay on the website
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|ts)$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader',
+        options: {
+          emitError: true,
+          emitWarning: true,
+          failOnError: true
+        }
+      },
+      {
+        test: /\.(js|ts)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [ 'file-loader',]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.pug$/,
+        use: ['pug-loader']
+      },
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.pug'
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  resolve: {
+    extensions: ['.js', '.ts']
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
