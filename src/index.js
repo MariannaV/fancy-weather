1820e145b43b80897faef0b08e74c8b0552d08da
marianna <vergunmari@gmail.com>
2020-05-28 01:03:14 +0300

    feat: get location by city

--- src/index.js
+++ src/index.js
@@ -2,21 +2,38 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import './css/index.scss';
 
+async function render(currentLocation) {
+  createLocationBlock(currentLocation);
+  await API_weather.getWeather(currentLocation);
+  createWeatherTodayBlock(API_weather.dataWeatherToday);
+  createWeatherOfSomeDays(API_weather.dataWeatherOfSomeDays);
+  googleMapInit(currentLocation);
+  googleMapAPI.toggleLocation(currentLocation)
+  addCoordinates(currentLocation);
+  // changeBackgroundImage(API_images.receivedImage)
+}
 
 window.onload = async () => {
+  listenSearchForm();
   backgroundImageToggleButtonHandler();
   await API_geolocation.getLocation();
-  createLocationBlock(API_geolocation.currentLocation);
-  await API_weather.getWeather(API_geolocation.currentLocation);
-  await createWeatherTodayBlock(API_weather.dataWeatherToday);
-  createWeatherOfSomeDays(API_weather.dataWeatherOfSomeDays);
-  googleMapInit(API_geolocation.coordinates);
-  addCoordinates(API_geolocation.coordinates);
-  changeBackgroundImage(API_images.receivedImage)
+  render(API_geolocation.currentLocation);
 };
 
+const googleMapAPI = {
+  get map() {
+    // if (nonExist) then init
+    //  return map
+  },
+  toggleLocation(params) {
+    // this.map.toggle(params)
+  }
+}
 
-function googleMapInit(coordinates) {
+//TODO: must to call only first time, then we need to toggle cords and marker in google.maps
+function googleMapInit({ coordinates }) {
+
+  // if (!mapNonExist) {
   const googleToken = 'AIzaSyAtMzLExZ-4fG_3BBaeIgPStExfwLxwerw';
   const language = 'ru';
   const googleMapScript = document.createElement('script');
@@ -27,9 +44,11 @@ function googleMapInit(coordinates) {
   globalThis[googleMapInit.name] = googleMapInit;
 
   document.head.appendChild(googleMapScript);
+  // } else map.toggle(coords)
 
   function googleMapInit() {
     const { lat, lng } = coordinates;
+    //need call this map
     const map = new google.maps.Map(document.getElementById('map'), {
       center: {
         lat,
@@ -48,7 +67,7 @@ function googleMapInit(coordinates) {
 }
 
 
-function addCoordinates(coordinates){
+function addCoordinates({ coordinates } ) {
   const { lat, lng } = coordinates;
   document.querySelector('.map-section .coordinates').insertAdjacentHTML(
     'beforeend',
@@ -62,7 +81,6 @@ export const API_geolocation = {
   get apikey() {
     return 'a4afdd31e79510';
   },
-  coordinates: {},
   currentLocation: {},
   async getLocation(params = {}) {
     try {
@@ -71,23 +89,41 @@ export const API_geolocation = {
         throw Error('Something went wrong');
       }
       const result = await response.json();
-      this.currentLocation = result;
+
       const locationCoordinatesArray = result.loc.split(',');
-      this.coordinates = {
-        lat: Number(locationCoordinatesArray[0]),
-        lng: Number(locationCoordinatesArray[1])
-      }
+      this.currentLocation = {
+        ...result,
+        coordinates: {
+          lat: Number(locationCoordinatesArray[0]),
+          lng: Number(locationCoordinatesArray[1])
+        }
+      };
     } catch (error) {
       alert(`error : ${error}`);
     }
+  },
+  async getLocationByCity({city}) {
+    const token = '3c0960e747d4430daf05b9de5716302a';
+    const response = await fetch(`https://api.opencagedata.com/geocode/v1/json?q=${city}&key=${token}`);
+    if (!response.ok) {
+      throw Error('Something went wrong');
+    }
+    const { results } = await response.json()
+    if (!results.length) {
+      throw Error('No match');
+    }
+    const bestMatch = results[0];
+    return {
+      city: bestMatch.components.city ?? city,
+      country: bestMatch.components.country,
+      coordinates: bestMatch.geometry
+    }
   }
-
-
 };
 
 function createLocationBlock(locationData) {
   const { city, country } = locationData;
-  console.log(2, API_geolocation.currentLocation);
+  // console.log(2, API_geolocation.currentLocation);
   const location = document.querySelector('.header-block .location');
   location.innerHTML = '';
   location.insertAdjacentHTML('beforeend',
@@ -103,32 +139,53 @@ const API_weather = {
   get apikey() {
     return '69608718e01c4ff1e36fa29958bb43b6';
   },
-
-  dataWeather: [],
-  dataWeatherToday: {},
-  dataWeatherOfSomeDays: [],
+  currentCity: null,
+  dataWeather: new Map(),
+  get dataWeatherToday() {
+    return this.dataWeather.get(this.currentCity)?.[0];
+  },
+  get dataWeatherOfSomeDays() {
+    return this.dataWeather.get(this.currentCity)?.slice(1);
+  },
   async getWeather(params) {
     const { city, lang = 'eng' } = params;
     try {
+      this.currentCity = city;
+      const amountOfForecastDays = 4;
+      const hasAllDataAlready = this.dataWeather.get(city)?.length >= amountOfForecastDays;
+      if (hasAllDataAlready) return;
+
       const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&lang=${lang}g&units=metric&APPID=${this.apikey}`);
       if (!response.ok) {
         throw Error('Something went wrong');
       }
       const result = await response.json();
-      const amountOfForecastDays = 4;
-      for (let i = 0; i<amountOfForecastDays && i < result.list.length; i+=1){
+
+      if (!this.dataWeather.has(city)) this.dataWeather.set(city, []);
+
+      for (let i = 0; i < amountOfForecastDays && i < result.list.length; i += 1) {
         const resultItem = result.list[i];
-        this.dataWeather.push({ weather: resultItem.weather[0].description,degree : Math.round(resultItem.main.temp), feelsLike : Math.round(resultItem.main.feels_like), wind : resultItem.wind.speed, humidity: resultItem.main.humidity});
+        this.dataWeather.get(city).push({
+          weather: resultItem.weather[0].description,
+          degree: Math.round(resultItem.main.temp),
+          feelsLike: Math.round(resultItem.main.feels_like),
+          wind: resultItem.wind.speed,
+          humidity: resultItem.main.humidity
+        });
       }
-      this.dataWeatherToday = this.dataWeather[0];
-      this.dataWeatherOfSomeDays = this.dataWeather.splice(1);
-      console.log(3, result, this.dataWeather, 'list', result.list, 'weather TODAY', this.dataWeatherToday, 'weather of some days', this.dataWeatherOfSomeDays);
     } catch (error) {
-      alert(`error : ${error}`);
+      if (error.message = 'city not found') {
+        const searchInput = document.querySelector('#searchForm .search-input');
+        searchInput.value = '';
+        searchInput.setAttribute('placeholder', 'Type correct data');
+      } else {
+        alert(`error : ${error}`);
+      }
     }
   }
 
 };
+
 function createDate() {
   const date = new Date();
 
@@ -169,7 +226,7 @@ function calcFahrenheitDegrees(celsiusDegree) {
 
 const API_images = {
   get apikey() {
-    return '_cXf5V9ZeOttjJE2clN9URiApDrCRiB8g2frf30AS-M'
+    return '_cXf5V9ZeOttjJE2clN9URiApDrCRiB8g2frf30AS-M';
   },
   receivedImage: '',
   async getImageUrl() {
@@ -192,12 +249,12 @@ const API_images = {
 async function changeBackgroundImage() {
   await API_images.getImageUrl();
   const url = API_images.receivedImage;
-  document.body.setAttribute("style", `background-image:  linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5)), url(' ${url}');`);
+  document.body.setAttribute('style', `background-image:  linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.5)), url(' ${url}');`);
   console.log('url2', url);
 }
 
 function backgroundImageToggleButtonHandler() {
-  const  backgroundImageToggleButton = document.querySelector('.toggle-background-image');
+  const backgroundImageToggleButton = document.querySelector('.toggle-background-image');
   backgroundImageToggleButton.addEventListener('click', changeBackgroundImage);
 }
 
@@ -217,9 +274,9 @@ function degreesHandler() {
 degreesHandler();
 
 function createWeatherTodayBlock(dataWeatherToday) {
-  console.log('DW',dataWeatherToday);
+  console.log('DW', dataWeatherToday);
   // const dataWeatherToday = dataWeather[0];
-  const {weather, degree, feelsLike, wind, humidity} = dataWeatherToday;
+  const { weather, degree, feelsLike, wind, humidity } = dataWeatherToday;
   const weatherTodayBlock = document.querySelector('.about-weather .weather-today-block');
   weatherTodayBlock.innerHTML = '';
   weatherTodayBlock.insertAdjacentHTML('beforeend',
@@ -231,22 +288,39 @@ function createWeatherTodayBlock(dataWeatherToday) {
       <p>wind: ${wind} m/s</p>
       <p>humidity: ${humidity}%</p>
     </div>
-    `)
+    `);
 }
 
-function createWeatherOfSomeDays(dataWeatherOfSomeDays){
+function createWeatherOfSomeDays(dataWeatherOfSomeDays) {
   const forecastBlock = document.querySelector('.about-weather .forecast-of-some-days');
-dataWeatherOfSomeDays.forEach(dataOfWeatherDay => {
-  forecastBlock.insertAdjacentHTML('beforeend',
-    `
+  forecastBlock.innerHTML = '';
+  dataWeatherOfSomeDays.forEach(dataOfWeatherDay => {
+    forecastBlock.insertAdjacentHTML('beforeend',
+      `
     <div class="weather-of-day">
         <p>${dataOfWeatherDay.degree}°</p>
         <p>${dataOfWeatherDay.weather}</p>
     </div>
     `
-    )
+    );
 
-})
+  });
+};
 
 
-}
+function listenSearchForm() {
+  const searchForm = document.getElementById('searchForm');
+  const searchInput = searchForm.querySelector('.search-input');
+  searchForm.addEventListener('submit', async (button) => {
+    button.preventDefault();
+    const city=searchInput.value.trim();
+    const newLocation = await API_geolocation.getLocationByCity({ city})
+    render(newLocation)
+    // await fetch(`http://geodb-free-service.wirefreethought.com/v1/geo/cities?namePrefix=${city}&limit=5&offset=0&hateoasMode=false`)
+    //get currentLocation by city?
+    //
+    // await API_weather.getWeather({ city: searchInput.value.trim() });
+    // createWeatherTodayBlock(API_weather.dataWeatherToday);
+    // createWeatherOfSomeDays(API_weather.dataWeatherOfSomeDays);
+  });
+};
