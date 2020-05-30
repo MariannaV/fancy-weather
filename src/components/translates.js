ed74c7c2d145ee21ef7d2b058568404529bc6ddf
marianna <vergunmari@gmail.com>
2020-05-31 00:08:46 +0300

    feat: add data translation

---
+++ src/components/translates.js
@@ -0,0 +1,114 @@
+
+export const translates = {
+  en: {
+    latitude: 'Latitude',
+    longitude: 'Longitude',
+    daysOfWeek: {
+      monday: 'Monday',
+      tuesday: 'Tuesday',
+      wednesday: 'Wednesday',
+      thursday: 'Thursday',
+      friday: 'Friday',
+      saturday: 'Saturday',
+      sunday: 'Sunday'
+    },
+    weather: {
+      feelsLike: 'feels like',
+      wind: 'wind',
+      humidity: 'humidity'
+    },
+    months: {
+      january: 'January',
+      february: 'February',
+      march: 'March',
+      april: 'April',
+      may: 'May',
+      june: 'June',
+      july: 'July',
+      august: 'August',
+      september: 'September',
+      october: 'October',
+      november: 'November',
+      december: 'December'
+    },
+    searchFormData: {
+      errorMessage: 'Incorrect data',
+      searchInputPlaceholder: 'Search city',
+      buttonText: 'Search',
+    }
+  },
+  ru: {
+    latitude: 'Ширина',
+    longitude: 'Долгота',
+    daysOfWeek: {
+      monday: 'Понедельник',
+      tuesday: 'Вторник',
+      wednesday: 'Среда',
+      thursday: 'Четверг',
+      friday: 'Пятница',
+      saturday: 'Суббота',
+      sunday: 'Воскресенье'
+    },
+    weather: {
+      feelsLike: 'ощущается как',
+      wind: 'ветер',
+      humidity: 'влажность'
+    },
+    months: {
+      january: 'Января',
+      february: 'Февраля',
+      march:'Марта',
+      april: 'Апреля',
+      may: 'Мая',
+      june: 'Июня',
+      july: 'Июля',
+      august: 'Августа',
+      september: 'Сентября',
+      october: 'Октября',
+      november: 'Ноября',
+      december: 'Декабря'
+    },
+    searchFormData: {
+      errorMessage: 'Неверные данные',
+      searchInputPlaceholder: 'Найти город',
+      buttonText: 'Поиск',
+    }
+  },
+  be: {
+    latitude: 'Шыраты',
+    longitude: 'Даўгата',
+    daysOfWeek: {
+      monday: 'Панядзелак',
+      tuesday: 'Аўторак',
+      wednesday: 'Серада',
+      thursday: 'Чацвер',
+      friday: 'Пятніца',
+      saturday: 'Субота',
+      sunday: 'Нядзеля'
+    },
+    weather: {
+      feelsLike: 'адчуваецца як',
+      wind: 'вецер',
+      humidity: 'вільготнасць'
+    },
+    months: {
+      january: 'Студзень',
+      february: 'Лютага',
+      march: 'Сакавік',
+      april: 'Красавік',
+      may: 'Май',
+      june: 'Чэрвень',
+      july: 'Ліпеня',
+      august: 'Жнівень',
+      september: 'Верасень',
+      october: 'Кастрычнік',
+      november: 'Лістапада',
+      december: 'Снежні'
+    },
+    searchFormData: {
+      errorMessage: 'Няслушныя дадзеныя',
+      searchInputPlaceholder: 'Знайсці горад',
+      buttonText: 'Пошук',
+    }
+  }
+};
